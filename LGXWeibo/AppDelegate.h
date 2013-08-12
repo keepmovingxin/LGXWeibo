6fc9fcde23f26086dbfef2852c494a7c2cd09fd7
admin <admin@admintekiMac.local>
2013-08-12 11:25:36 +0800

    Initial Commit

---
+++ LGXWeibo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LGXWeibo
+//
+//  Created by admin on 13-8-12.
+//  Copyright (c) 2013年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
